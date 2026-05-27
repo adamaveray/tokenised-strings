92f166abcebe7434413e12dd5e7ec10b7f785ccd
Adam Averay <adam@averay.com>
2026-05-27 15:08:12 +0100

    Fix nullable argument deprecations

--- src/Exceptions/InaccessibleNestedValueException.php
+++ src/Exceptions/InaccessibleNestedValueException.php
@@ -5,7 +5,7 @@ namespace Averay\TokenisedStrings\Exceptions;
 
 class InaccessibleNestedValueException extends \OutOfBoundsException
 {
-  public function __construct(string $message, string $propertyPath, \Throwable $previous = null)
+  public function __construct(string $message, string $propertyPath, ?\Throwable $previous = null)
   {
     $message .= sprintf(' (at path "%s")', $propertyPath);
     parent::__construct($message, 0, $previous);
--- src/Exceptions/ParsingException.php
+++ src/Exceptions/ParsingException.php
@@ -5,7 +5,7 @@ namespace Averay\TokenisedStrings\Exceptions;
 
 class ParsingException extends \RuntimeException
 {
-  public function __construct(string $message, string $string, int $offset, \Throwable $previous = null)
+  public function __construct(string $message, string $string, int $offset, ?\Throwable $previous = null)
   {
     $message .= sprintf(' (at offset %d of "%s")', $offset, $string);
     parent::__construct($message, 0, $previous);
